b5163c315728e769c5feb011b282b215b6fe44e1
eoinahern <eoin.ahern@mycit.ie>
2019-05-29 21:26:32 +0300

    added toolbar stuff  and opacity change

--- app/src/main/java/eoinahern/ie/movietrailerapp/ui/view/detail/DetailActivity.kt
+++ app/src/main/java/eoinahern/ie/movietrailerapp/ui/view/detail/DetailActivity.kt
@@ -2,20 +2,51 @@ package eoinahern.ie.movietrailerapp.ui.view.detail
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.appbar.AppBarLayout
 import eoinahern.ie.movietrailerapp.R
 import eoinahern.ie.movietrailerapp.ui.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        appBarAnimation()
+        toolbarSetup()
     }
 
     override fun getLayout() = R.layout.activity_detail
 
     companion object {
         fun getStartIntent(context: Context): Intent = Intent(context, DetailActivity::class.java)
+    }
+
+
+    private fun toolbarSetup() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back_24dp)
+    }
+
+
+    fun appBarAnimation() {
+
+        appbar.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
+            var scrollRange = -1
+
+            override fun onOffsetChanged(appBar: AppBarLayout?, offset: Int) {
+                if (scrollRange == -1) {
+                    scrollRange = appBar?.totalScrollRange ?: 0
+                }
+
+                val percent = Math.abs(offset) / scrollRange.toFloat()
+                toolbar.alpha = percent
+                image.alpha = 1.00f - percent
+            }
+
+
+        })
+
     }
 }
