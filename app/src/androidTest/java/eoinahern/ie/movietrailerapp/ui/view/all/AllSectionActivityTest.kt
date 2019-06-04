6e939930640ea18cfd0b91e8142190860d47a943
eoinahern <eoin.ahern@mycit.ie>
2019-06-04 14:51:35 +0300

    messing around with espresso

---
+++ app/src/androidTest/java/eoinahern/ie/movietrailerapp/ui/view/all/AllSectionActivityTest.kt
@@ -0,0 +1,12 @@
+package eoinahern.ie.movietrailerapp.ui.view.all
+
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class AllSectionActivityTest {
+
+    @Before
+    fun setUp() {
+    }
+}
---
+++ app/src/androidTest/java/eoinahern/ie/movietrailerapp/ui/view/detail/DetailActivityTest.kt
@@ -0,0 +1,12 @@
+package eoinahern.ie.movietrailerapp.ui.view.detail
+
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class DetailActivityTest {
+
+    @Before
+    fun setUp() {
+    }
+}
